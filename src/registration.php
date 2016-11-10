e359bb0e29597b65d08b29340ec722dc61bd937e
Marta Siewierska <marta.siewierska-stanczak@creativestyle.pl>
2016-11-10 09:01:10 +0100

    composer json

--- src/registration.php
+++ src/registration.php
@@ -5,6 +5,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Creativestyle/template-boilerplate',
+    'frontend/Creativestyle/magento2-template-creativeshop',
     __DIR__
 );
