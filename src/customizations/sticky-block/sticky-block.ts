1fe9f35f4ae82bdf945cb9208b30761b4cded8d8
Marta Siewierska <marta.siewierska-stanczak@creativestyle.pl>
2016-11-10 09:01:10 +0100

    Dashboard pages, layouts, templates changes and introduction of needed components: [flyout, dropdown, stack-nav, t-headline, t-text, link, visually-hidden, info-block, title-block, wishlist, customizations of inputs, buttons]

---
+++ src/customizations/sticky-block/sticky-block.ts
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-new */
+import $ from 'jquery';
+
+import StickyBlock from '../../../node_modules/creative-patterns/packages/components/sticky-block/src/sticky-block';
+
+/**
+ * StickyBlock component initialization for template
+ */
+new StickyBlock( $( '.cs-sticky-block' ) );
+new StickyBlock( $( '.cs-sticky-block-no-mobile' ) );
