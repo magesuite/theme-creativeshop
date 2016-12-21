ef2bc531222f7253e06eda18bab9722a17dbf357
Marta Siewierska <marta.siewierska-stanczak@creativestyle.pl>
2016-12-21 09:32:01 +0100

    Basket

--- src/customizations/sticky-block/sticky-block.ts
+++ src/customizations/sticky-block/sticky-block.ts
@@ -1,10 +1,13 @@
 /* tslint:disable:no-unused-new */
-
 import StickyBlock from '../../../node_modules/creative-patterns/packages/components/sticky-block/src/sticky-block';
+import breakpoint from '../../../node_modules/creative-patterns/packages/utilities/breakpoint/src/breakpoint';
 import $ from 'jquery';
 
 /**
  * StickyBlock component initialization for template
  */
 new StickyBlock( $( '.cs-sticky-block' ) );
-new StickyBlock( $( '.cs-sticky-block-no-mobile' ) );
+
+new StickyBlock( $( '.cs-sticky-block-no-mobile' ), {
+    breakpoint: breakpoint.tablet,
+} );
