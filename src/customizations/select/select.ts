20f9b77679852e649311c793b7b06b6693d1b1be
Maciej Borkowski <maciej.borkowski@creativestyle.pl>
2016-12-29 14:39:38 +0100

    Grid-product and products-grid for category page; select component implemented for sorter on category page

---
+++ src/customizations/select/select.ts
@@ -0,0 +1,6 @@
+/* tslint:disable:no-unused-expression no-unused-new */
+
+import Select from '../../../node_modules/creative-patterns/packages/components/select/src/select';
+import $ from 'jquery';
+
+new Select( $( '.cs-select' ) );
