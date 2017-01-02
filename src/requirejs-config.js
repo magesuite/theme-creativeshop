12eac32ebe750ea0b9bf9b651af61e6a6acf4dae
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2017-01-02 10:58:39 +0100

    Add better JS handling. Add watch task for better workflow.

---
+++ src/requirejs-config.js
@@ -0,0 +1,29 @@
+var config = {
+    paths: {
+        'Swiper': 'swiper',
+        'vendors': 'vendors',
+        'Stickyfill': 'stickyfill',
+        'isMobile': 'ismobile',
+    },
+    shim: {
+        'vendors': {
+            deps: [
+                'jquery',
+            ],
+        },
+        'Stickyfill': {
+            deps: [
+                'jquery',
+            ],
+            exports: 'Stickyfill',
+        },
+        'isMobile': {
+            exports: 'isMobile',
+        },
+    },
+    map: {
+        '*': {
+            'mage/validation': 'js/validation',
+        }
+    },
+};
