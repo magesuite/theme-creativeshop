e342d1378d0825cedfd2767f2ec9e93fbf321b91
Maciej Borkowski <maciej.borkowski@creativestyle.pl>
2017-01-03 14:02:25 +0100

    [PDP] New XML layout and styling; star rating widget improved by text-feedback while rating; new swatches

---
+++ src/customizations/hero/hero.ts
@@ -0,0 +1,6 @@
+/* tslint:disable:no-unused-expression no-unused-new */
+
+import Hero from '../../../node_modules/creative-patterns/packages/components/hero/src/hero';
+import $ from 'jquery';
+
+new Hero( $( '.cs-hero' ) );
---
+++ src/customizations/products-promo/products-promo.ts
@@ -0,0 +1,8 @@
+/* tslint:disable:no-unused-expression no-unused-new */
+
+import ProductsPromo from '../../../node_modules/creative-patterns/packages/components/products-promo/src/products-promo';
+import $ from 'jquery';
+
+new ProductsPromo( $( '.cs-products-promo' ), {
+    slideMinWidth: 240,
+} );
