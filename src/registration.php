79af17f5fd3a4c87cddb1792dc3dc69205304de1
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2017-01-21 08:34:40 +0000

    Update registration.php

--- src/registration.php
+++ src/registration.php
@@ -5,6 +5,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Creativestyle/magento2-template-creativeshop',
+    'frontend/Creativestyle/theme-creativeshop',
     __DIR__
 );
