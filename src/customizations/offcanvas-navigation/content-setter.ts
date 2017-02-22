0db12b1d311dbfac0c732727acc5ef4e6f0987c6
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2017-02-22 09:32:03 +0100

    Fix typo in PHTML.

--- src/customizations/offcanvas-navigation/content-setter.ts
+++ src/customizations/offcanvas-navigation/content-setter.ts
@@ -10,6 +10,7 @@ export interface NavigationTree {
 
 const navClassName: string = 'cs-navigation';
 const offNavClassName: string = 'cs-offcanvas-navigation';
+const infoStripeClassName: string = 'cs-information-stripe';
 
 /**
  * Builds subtree structure from existing desktop navigation.
@@ -50,12 +51,19 @@ export function buildTree( $links: JQuery ): NavigationTree[] {
  * @param  {NavigationTree}   parent         Parent category.
  * @return {string}                          Rendered HTML.
  */
-export function renderTree( navigationTree: NavigationTree[], parent?: NavigationTree ) {
+export function renderTree( navigationTree: NavigationTree[], parent?: NavigationTree ): string {
     let subtreeHTML: string = parent ? `<ul class="${offNavClassName}__list">` : '';
     navigationTree.forEach((category: NavigationTree, index: number) => {
         if ( parent && index === 0 ) {
             subtreeHTML += `<li class="${offNavClassName}__item">
-                <a class="${offNavClassName}__link ${offNavClassName}__link--return" href="${parent.url}">${parent.name}</a>
+                <a class="${offNavClassName}__link ${offNavClassName}__link--return" href="${parent.url}">
+                    <svg class="${offNavClassName}__icon">
+                        <use xlink:href="#arrow_prev"></use>
+                    </svg>
+                    <span class="${offNavClassName}__text">
+                        ${parent.name}
+                    </span>
+                </a>
             </li>
             <li class="cs-offcanvas-navigation__item">
                 <a class="cs-offcanvas-navigation__link" href="${parent.url}">${$translate('All products')}</a>
@@ -64,26 +72,55 @@ export function renderTree( navigationTree: NavigationTree[], parent?: Navigatio
 
         subtreeHTML += `<li class="${offNavClassName}__item">`;
         if (category.subcategories.length) {
-            subtreeHTML +=`<a class="${offNavClassName}__link ${offNavClassName}__link--parent" href="${category.url}">${category.name}</a>
+            subtreeHTML += `<a class="${offNavClassName}__link ${offNavClassName}__link--parent" href="${category.url}">
+                <span class="${offNavClassName}__text">
+                    ${category.name}
+                </span>
+                <svg class="${offNavClassName}__icon">
+                    <use xlink:href="#arrow_next"></use>
+                </svg>
+            </a>
                 ${renderTree(category.subcategories, category)}`;
         } else {
-            subtreeHTML += `<a class="${offNavClassName}__link" href="${category.url}">${category.name}</a>`;
+            subtreeHTML += `<a class="${offNavClassName}__link" href="${category.url}">
+                <span class="${offNavClassName}__text">
+                    ${category.name}
+                </span>
+            </a>`;
         }
         subtreeHTML += '</li>';
     });
-    subtreeHTML += parent ? '</ul>': '';
+    subtreeHTML += parent ? '</ul>' : '';
 
     return subtreeHTML;
+}
+
+export function renderUserAction(): string {
+    const $accountLink: JQuery = $( `.${infoStripeClassName}__account-link` );
+    const accountLinkHref: string = $accountLink.attr( 'href' );
+    const accountLinkText: string = $accountLink.text();
+    const accountActionType: string = accountLinkText.indexOf( 'out' ) === -1 ? 'in' : 'out';
+
+    return `<li class="${offNavClassName}__item">
+        <a class="${offNavClassName}__link ${offNavClassName}__link--sign-${accountActionType}" href="${accountLinkHref }">
+            <span class="${offNavClassName}__text">
+                ${accountLinkText}
+            </span>
+            <svg class="${offNavClassName}__icon">
+                <use xlink:href="#sign-${accountActionType}"></use>
+            </svg>
+        </a>
+    </li>`;
 }
 
 /**
  * Sets offcanvas navigation content.
  */
-export const contentSetter = ( offcanvasNavigation: OffcanvasNavigation ): void => {
+export const contentSetter: Function = ( offcanvasNavigation: OffcanvasNavigation ): void => {
     const $navigation: JQuery = $( `.${navClassName}` );
     const $links: JQuery = $navigation.find( `.${navClassName}__link` ).not( `.${navClassName}__link--category, .${navClassName}__link--subcategory` );
-    const $offNavList = offcanvasNavigation.getElement().find(`.${offNavClassName}__list`);
-    $offNavList.append(renderTree(buildTree($links)));
+    const $offNavList: JQuery = offcanvasNavigation.getElement().find( `.${offNavClassName}__list` );
+    $offNavList.append( renderUserAction() + renderTree( buildTree( $links ) ) );
 };
 
 export default contentSetter;
