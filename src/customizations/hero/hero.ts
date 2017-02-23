b0fde79fa1089a9f544ed2ad15191a457afd8bd7
Marta Siewierska <marta.siewierska-stanczak@creativestyle.pl>
2017-02-23 09:31:14 +0100

    Adding opengraph blocks, adding canonical to every page, change duplicated alt on hero slides, remove duplicated id-s

--- src/customizations/hero/hero.ts
+++ src/customizations/hero/hero.ts
@@ -5,6 +5,15 @@ import $ from 'jquery';
 
 $( '.cs-hero' ).each( function(): void {
     new Hero( $( this ), {
-        spaceBetween: 2
+        spaceBetween: 2,
+        callbacks: { 
+            onInit: function() { 
+                $( `.cs-hero` ).find( `.cs-hero__slide--clone` ).each( function( i: number, el: JQuery ) { 
+                    let clonedSlideImg = $(el).find( 'img' ); 
+                    let clonedSlideImgAlt = clonedSlideImg.attr( 'alt' ); 
+                    clonedSlideImg.attr( 'alt', `${clonedSlideImgAlt}-duplicated-${i}`); 
+                } ); 
+            } 
+        }
     } );
 } );
