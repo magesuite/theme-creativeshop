90a5a0a56577febb3b1fb3b8b1796d8d31aca967
Marta Siewierska <marta.siewierska-stanczak@creativestyle.pl>
2017-04-07 09:35:13 +0200

    Checkout adjustments

--- src/Magento_Ui/web/js/form/element/post-code.js
+++ src/Magento_Ui/web/js/form/element/post-code.js
@@ -17,10 +17,43 @@ define([
             }
         },
 
+        // REF-PATCH START avoid premature ZIP code validation (by using less strict compare method),
+        /**
+         * Initializes observable properties of instance
+         *
+         * @returns {Abstract} Chainable.
+         */
+        initObservable: function() {
+            this._super();
+            this.value.equalityComparer = function( a, b ) {
+                return (!a && !b) || (a == b);
+            };
+            return this;
+        },
+        // REF-PATCH END
+
         /**
          * @param {String} value
          */
-        update: function (value) {
+        update: function( value ) {
+            var country = registry.get( this.parentName + '.' + 'country_id' ),
+                options = country.indexedOptions,
+                option;
+
+            if ( !value ) {
+                return;
+            }
+
+            option = options[ value ];
+
+            if ( option[ 'is_zipcode_optional' ] ) {
+                this.error( false );
+                this.validation = _.omit( this.validation, 'required-entry' );
+            } else {
+                this.validation[ 'required-entry' ] = true;
+            }
+
+            this.required( !option[ 'is_zipcode_optional' ] );
         }
     });
 });
