0f0f7eb52bb8c1bc9b1b208ec3bff6df2dadad6b
Maciej Borkowski <maciej.borkowski@creativestyle.pl>
2017-04-18 08:49:47 +0200

    Better control of some teaser scrips

--- src/customizations/brand-carousel/brand-carousel.ts
+++ src/customizations/brand-carousel/brand-carousel.ts
@@ -3,30 +3,32 @@
 import BrandCarousel from '../../../node_modules/creative-patterns/packages/components/brand-carousel/src/brand-carousel';
 import $ from 'jquery';
 
-new BrandCarousel( $( '.cs-brand-carousel' ), {
-    spaceBetween: 50,
-    slidesPerView: 6,
-    centeredSlides: false,
-    slideMinWidth: 50,
-    roundLengths: false,
-    paginationBreakpoint: 10,
-    breakpoints: {
-        380: {
-            slidesPerView: 2,
-            spaceBetween: 25,
+if ( $( '.cs-brand-carousel' ).length ) {
+    new BrandCarousel( $( '.cs-brand-carousel' ), {
+        spaceBetween: 50,
+        slidesPerView: 6,
+        centeredSlides: false,
+        slideMinWidth: 50,
+        roundLengths: false,
+        paginationBreakpoint: 10,
+        breakpoints: {
+            380: {
+                slidesPerView: 2,
+                spaceBetween: 25,
+            },
+            480: {
+                slidesPerView: 3,
+                spaceBetween: 35,
+            },
+            640: {
+                slidesPerView: 4,
+            },
+            768: {
+                slidesPerView: 5,
+            },
+            960: {
+                slidesPerView: 5,
+            },
         },
-        480: {
-            slidesPerView: 3,
-            spaceBetween: 35,
-        },
-        640: {
-            slidesPerView: 4,
-        },
-        768: {
-            slidesPerView: 5,
-        },
-        960: {
-            slidesPerView: 5,
-        },
-    },
-} );
+    } );
+}
