f9d76a18739c4006f881d90cdf3a392720154f0c
Marta Siewierska <marta.siewierska-stanczak@creativestyle.pl>
2017-05-04 10:06:58 +0200

    Change way to detect payments step of the checkout

--- src/Magento_Checkout/web/js/before-place-order.js
+++ src/Magento_Checkout/web/js/before-place-order.js
@@ -1,14 +1,12 @@
-/**
- * Copyright © 2016 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
+
 /*global define*/
 define(
     [
         'uiComponent',
-        'Magento_Checkout/js/model/step-navigator'
+        'Magento_Checkout/js/model/step-navigator',
+        'Magento_Checkout/js/model/quote'
     ],
-    function (Component, stepNavigator ) {
+    function (Component, stepNavigator, quote) {
         "use strict";
         return Component.extend({
             defaults: {
@@ -17,7 +15,7 @@ define(
             },
 
             isVisible: function() {
-                return stepNavigator.getActiveItemIndex() == 1;
+                return !quote.isVirtual() && stepNavigator.isProcessed( 'shipping' );
             }
         });
     }
