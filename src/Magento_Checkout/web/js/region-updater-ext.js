ce10246c67c7097b236697c8f6fc7ec1be222cc3
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2017-12-01 15:20:54 +0100

    Restore state/province field hiding. Hide it when disabled.

---
+++ src/Magento_Checkout/web/js/region-updater-ext.js
@@ -0,0 +1,35 @@
+/**
+ * Region updater extension which is responsible for toggling whole input section
+ * including our custom containers and labels when selected country changes.
+ *
+ * Desired functionality includes:
+ * - Showing entire "State/Province" field when it is required.
+ * - Hiding it when it is not.
+ */
+define(['jquery', 'jquery/ui'], function($) {
+    'use strict';
+
+    return function(regionUpdater) {
+        return $.widget('mage.regionUpdater', regionUpdater, {
+            _updateRegion: function(country) {
+                this._super(country);
+
+                var $regionInput = $(this.options.regionInputId);
+                var $regionList = $(this.options.regionListId);
+
+                if (
+                    $regionInput.css('display') !== 'none' ||
+                    $regionList.prop('disabled')
+                ) {
+                    $regionList
+                        .closest('.cs-input')
+                        .addClass('cs-visually-hidden');
+                } else {
+                    $regionList
+                        .closest('.cs-input')
+                        .removeClass('cs-visually-hidden');
+                }
+            },
+        });
+    };
+});
