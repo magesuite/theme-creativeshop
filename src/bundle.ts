c04fe6776dc4ec69702c7cc1ffbaad380f4fc431
Marta Siewierska <marta.siewierska-stanczak@creativestyle.pl>
2018-01-03 08:03:29 +0100

    Make minicart flyout smaller on tablets

--- src/bundle.ts
+++ src/bundle.ts
@@ -54,6 +54,18 @@ import './customizations/grid-layout/grid-layout';
 import AddressAutofill from './customizations/address-autofill/address-autofill';
 export { AddressAutofill };
 
+
+// Sometimes there is a need to apply different styling for mobile/tablet devices and body class is necessary
+if (isMobile.any) {
+    document.body.classList.add('is-mobile');
+}
+
+if (isMobile.tablet) {
+    document.body.classList.add('is-tablet');
+} else if (isMobile.phone) {
+    document.body.classList.add('is-phone');
+}
+
 window.addEventListener(
     'touchstart',
     function onFirstTouch(): void {
