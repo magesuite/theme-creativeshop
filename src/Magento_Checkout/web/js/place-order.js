dda04a0d1fca4d02df36ed17426d616709c6fd94
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2018-01-31 10:03:05 +0100

    Add additional check for undefined value for checkout buttons

--- src/Magento_Checkout/web/js/place-order.js
+++ src/Magento_Checkout/web/js/place-order.js
@@ -1,18 +1,18 @@
 /*global define*/
-define(['uiComponent', 'Magento_Checkout/js/model/step-navigator'], function(
-    Component,
-    stepNavigator
-) {
-    'use strict';
-    return Component.extend({
-        defaults: {
-            displayArea: 'place-order',
-            template: 'Magento_Checkout/place-order',
-        },
-        isHidden: function() {
-            var shipping = stepNavigator.steps()[0];
+define(
+    ['ko', 'uiComponent', 'Magento_Checkout/js/model/step-navigator'],
+    function(ko, Component, stepNavigator) {
+        'use strict';
+        return Component.extend({
+            defaults: {
+                displayArea: 'place-order',
+                template: 'Magento_Checkout/place-order',
+            },
+            isHidden: function() {
+                var shipping = stepNavigator.steps()[0];
 
-            return shipping.isVisible;
-        },
-    });
-});
+                return shipping ? shipping.isVisible : ko.observable(false);
+            },
+        });
+    }
+);
