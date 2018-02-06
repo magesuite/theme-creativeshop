e29ca56d80fceb5af378023eb4baa8b809c29d7f
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2018-02-06 14:34:38 +0100

    Add support for more than 2 checkout steps to step detection.

--- src/Magento_Checkout/web/js/next-button.js
+++ src/Magento_Checkout/web/js/next-button.js
@@ -1,18 +1,20 @@
 /*global define*/
-define(['uiComponent', 'Magento_Checkout/js/model/step-navigator'], function(
-    Component,
-    stepNavigator
-) {
-    'use strict';
-    return Component.extend({
-        defaults: {
-            displayArea: 'next-button',
-            template: 'Magento_Checkout/next-button',
-        },
-        isHidden: function() {
-            var payment = stepNavigator.steps()[1];
+define(
+    ['ko', 'uiComponent', 'Magento_Checkout/js/model/step-navigator'],
+    function (ko, Component, stepNavigator) {
+        'use strict';
+        return Component.extend({
+            defaults: {
+                displayArea: 'next-button',
+                template: 'Magento_Checkout/next-button',
+            },
+            isHidden: function () {
+                var payment = stepNavigator.steps().filter(function (step) {
+                    return step.code === 'payment';
+                })[0];
 
-            return payment.isVisible;
-        },
-    });
-});
+                return payment ? payment.isVisible : ko.observable(false);
+            },
+        });
+    }
+);
--- src/Magento_Checkout/web/js/place-order.js
+++ src/Magento_Checkout/web/js/place-order.js
@@ -1,18 +1,19 @@
 /*global define*/
-define(['uiComponent', 'Magento_Checkout/js/model/step-navigator'], function(
-    Component,
-    stepNavigator
-) {
-    'use strict';
-    return Component.extend({
-        defaults: {
-            displayArea: 'place-order',
-            template: 'Magento_Checkout/place-order',
-        },
-        isHidden: function() {
-            var shipping = stepNavigator.steps()[0];
+define(
+    ['ko', 'uiComponent', 'Magento_Checkout/js/model/step-navigator'],
+    function (ko, Component, stepNavigator) {
+        'use strict';
+        return Component.extend({
+            defaults: {
+                displayArea: 'place-order',
+                template: 'Magento_Checkout/place-order',
+            },
+            isHidden: function () {
+                var shipping = stepNavigator.steps().filter(function (step) {
+                    return step.code === 'shipping';
+                })[0];
 
-            return shipping.isVisible;
-        },
+                return shipping.isVisible;
+            },
+        });
     });
-});
