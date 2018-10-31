dd74fb26b0ead01558a8460a19be4ebe560aeb0a
Maciej Borkowski <maciej.borkowski@creativestyle.pl>
2018-10-31 13:36:13 +0100

    Cleanup: remove unnecessary category-links scripts

--- src/components/category-links/category-links.ts
+++
@@ -1,9 +0,0 @@
-import * as $ from 'jquery';
-import { Flyout } from '../flyout/class.flyout';
-
-$('.cs-category-links-dropdown').each((index: number, element: any) => {
-    new Flyout($(element), {
-        name: 'cs-category-links-dropdown',
-        type: 'flyout',
-    });
-});
