d18f9bc15abd0b249f9aebc6c6b547780a5776c8
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2018-11-13 14:24:00 +0100

    Change builder to npm source

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const path = require('path');
 const gulp = require('gulp');
-const MagesuiteRegistry = require('magesuite-frontend-builder/registry');
+const MagesuiteRegistry = require('@creativestyle/magesuite-frontend-builder/registry');
 
 gulp.registry(MagesuiteRegistry);
