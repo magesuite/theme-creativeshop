a7de5921145b54a70974c25c67c1dbac314dcd94
Marta Siewierska <marta.siewierska-stanczak@creativestyle.pl>
2019-02-05 12:36:45 +0100

    Style checkout for logged in users + bugfixes

--- src/Magento_Checkout/web/js/view/shipping-ext.js
+++ src/Magento_Checkout/web/js/view/shipping-ext.js
@@ -7,12 +7,12 @@ define(['jquery'], function($) {
   return function(Shipping) {
     return Shipping.extend({
       enableForm: function() {
-        $('#shipping-new-address-form .cs-input').removeClass('cs-input--disabled').find('input,' +
+        $('#shipping-new-address-form input').removeAttr( 'disabled' ).find('input,' +
           ' ' +
           'select' ).removeAttr( 'disabled' );
       },
       disableForm: function() {
-        $('#shipping-new-address-form .cs-input').addClass('cs-input--disabled' ).find('input, ' +
+        $('#shipping-new-address-form .cs-input').attr( 'disabled', 'disabled').find('input, ' +
           'select').attr( 'disabled', 'disabled');
       }
 
