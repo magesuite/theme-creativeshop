9f27bd2b76edd6d362672bc1bb7fda696c103a2b
Tomasz Drabik <tomasz.drabik@creativetyle.pl>
2019-03-19 15:11:54 +0100

    Add bundles for: category pages, checkout, cms, customer area, pdp

---
+++ src/entries/cms.ts
@@ -0,0 +1,80 @@
+// =============================================================================
+// CMS pages entry point
+// It imports all the components required by CMS pages
+// Each page type has own entry point containing required components.
+// It allows optimizing bundles in webpack - which gathers common imports in separate package.
+// It is a job of every component to initilize itself.
+
+import 'config/base.scss';
+// TODO: Remove this file when possible
+import 'bundle.scss';
+
+// =============================================================================
+// Components
+// =============================================================================
+
+// Base components
+import 'components/authorization-link';
+import 'components/addtocart';
+import 'components/autocomplete'; //consider moving to ElasticSuite?
+import 'components/breadcrumbs';
+import 'components/button';
+import 'components/container';
+import 'components/cookie-message';
+import 'components/device-detection';
+import 'components/display-controller';
+import 'components/dropdown-switcher';
+import 'components/field'; // ??
+import 'components/footer';
+import 'components/grid-layout';
+import 'components/headline';
+import 'components/header';
+import 'components/lazyload';
+import 'components/links-block-addto'; // It is part of a navigation, why not there?
+import 'components/logo';
+import 'components/messages';
+import 'components/minicart';
+import 'components/minicart-product';
+import 'components/modal';
+import 'components/navigation';
+import 'components/newsletter';
+import 'components/offcanvas-toggle';
+import 'components/offcanvas-navigation';
+import 'components/offcanvas';
+import 'components/page-title';
+import 'components/page-bottom';
+import 'components/price-box';
+import 'components/product-tile';
+import 'components/slider';
+import 'components/social-media-list';
+import 'components/star-rating';
+import 'components/swatches';
+import 'components/tile-gallery';
+import 'components/topbar';
+import 'components/typography';
+import 'components/usps';
+import 'components/visually-hidden';
+
+// Content Constructor
+import 'components/brand-carousel';
+import 'components/category-links';
+import 'components/daily-deal-teaser';
+import 'components/dailydeal';
+import 'components/hero';
+import 'components/image-teaser';
+import 'components/image-teaser-legacy';
+import 'components/paragraph';
+import 'components/products-carousel';
+import 'components/products-grid';
+import 'components/product-finder';
+import 'components/separator';
+
+export { Select } from 'components/select';
+export { AddressAutofill } from 'components/address-autofill';
+
+// =============================================================================
+// Customizations
+// =============================================================================
+
+import 'customizations/plugincompany-contactforms/plugincompany-contactforms';
+import 'customizations/video-player/video-player';
---
+++ src/entries/pdp.ts
@@ -0,0 +1,94 @@
+// =============================================================================
+// Product Details Page entry point
+// It imports all the components required by PDP
+// Each page type has own entry point containing required components.
+// It allows optimizing bundles in webpack - which gathers common imports in separate package.
+// It is a job of every component to initilize.
+
+import 'config/base.scss';
+// TODO: Remove this file when possible
+import 'bundle.scss';
+
+// =============================================================================
+// Components
+// =============================================================================
+import 'pages/product';
+
+// Base components
+import 'components/authorization-link';
+import 'components/addtocart';
+import 'components/autocomplete'; //consider moving to ElasticSuite?
+import 'components/breadcrumbs';
+import 'components/button';
+import 'components/container';
+import 'components/cookie-message';
+import 'components/device-detection';
+import 'components/display-controller';
+import 'components/dropdown-switcher';
+import 'components/field'; // ??
+import 'components/footer';
+import 'components/grid-layout';
+import 'components/headline';
+import 'components/header';
+import 'components/lazyload';
+import 'components/links-block-addto'; // It is part of a navigation, why not there?
+import 'components/logo';
+import 'components/messages';
+import 'components/minicart';
+import 'components/minicart-product';
+import 'components/modal';
+import 'components/navigation';
+import 'components/newsletter';
+import 'components/offcanvas-toggle';
+import 'components/offcanvas-navigation';
+import 'components/offcanvas';
+import 'components/page-title';
+import 'components/page-bottom';
+import 'components/price-box';
+import 'components/product-tile';
+import 'components/slider';
+import 'components/social-media-list';
+import 'components/star-rating';
+import 'components/swatches';
+import 'components/tile-gallery';
+import 'components/topbar';
+import 'components/typography';
+import 'components/usps';
+import 'components/visually-hidden';
+// Content Constructor
+import 'components/brand-carousel';
+import 'components/category-links';
+import 'components/daily-deal-teaser';
+import 'components/dailydeal';
+import 'components/hero';
+import 'components/image-teaser';
+import 'components/image-teaser-legacy';
+import 'components/paragraph';
+import 'components/products-carousel';
+import 'components/products-grid';
+import 'components/product-finder';
+import 'components/separator';
+
+// PDP specific
+import 'components/authorization-link';
+import 'components/bundle-box';
+import 'components/buybox';
+import 'components/form';
+import 'components/indicators';
+import 'components/product-gallery';
+import 'components/product-details';
+import 'components/tabs';
+import 'components/qty-increment';
+import 'components/table';
+
+
+// Exported for usage in templates:
+export { Select } from 'components/select';
+export { AddressAutofill } from 'components/address-autofill';
+
+// =============================================================================
+// Customizations
+// =============================================================================
+
+import 'customizations/reviews/reviews'; // PDP/USER
+import 'customizations/plugincompany-contactforms/plugincompany-contactforms';
