c7fc5843afe39eb595757d114ef51d2d8922cb41
Tomasz Drabik <tomasz.drabik@creativetyle.pl>
2019-04-15 19:51:38 +0200

    Added entry and asset for contact page

---
+++ src/entries/contact.ts
@@ -0,0 +1,63 @@
+// =============================================================================
+// Contact page entry point
+// It imports all the components required by contact page.
+// Each page type has own entry point containing required components.
+// It allows optimizing bundles in webpack - which gathers common imports in separate package.
+// It is a job of every component to initilize itself.
+
+import 'config/base.scss';
+// TODO: Remove this file when possible
+import 'bundle.scss';
+
+// =============================================================================
+// Components
+// =============================================================================
+
+// Base components
+import 'components/authorization-link';
+import 'components/addtocart';
+import 'components/autocomplete';
+import 'components/breadcrumbs';
+import 'components/button';
+import 'components/container';
+import 'components/cookie-message';
+import 'components/device-detection';
+import 'components/display-controller';
+import 'components/dropdown-switcher';
+import 'components/field';
+import 'components/footer';
+import 'components/grid-layout';
+import 'components/headline';
+import 'components/header';
+import 'components/lazyload';
+import 'components/links-block-addto';
+import 'components/logo';
+import 'components/messages';
+import 'components/minicart';
+import 'components/minicart-product';
+import 'components/modal';
+import 'components/navigation';
+import 'components/newsletter';
+import 'components/offcanvas-toggle';
+import 'components/offcanvas-navigation';
+import 'components/offcanvas';
+import 'components/page-title';
+import 'components/page-bottom';
+import 'components/price-box';
+import 'components/product-tile';
+import 'components/slider';
+import 'components/social-media-list';
+import 'components/star-rating';
+import 'components/swatches';
+import 'components/topbar';
+import 'components/typography';
+import 'components/select';
+// Contact page specific
+import 'components/captcha';
+import 'components/form';
+
+import 'customizations/plugincompany-contactforms/plugincompany-contactforms';
+
+// Exported for usage in templates:
+export { Select } from 'components/select';
+export { AddressAutofill } from 'components/address-autofill';
