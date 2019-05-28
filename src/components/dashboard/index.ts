058377d541818e971fb680b0e3334f299de0f7fb
Piotr Tabor <piotr.tabor@creativestyle.pl>
2019-05-28 18:57:01 +0200

    half way through

--- src/components/dashboard/index.ts
+++ src/components/dashboard/index.ts
@@ -1,44 +1,4 @@
 import 'components/dashboard/dashboard.scss';
-import breakpoint from 'utils/breakpoint/breakpoint';
-import { SingleOrMany } from 'stickyfilljs';
+// import Sticky from 'components/sticky/sticky';
 
-// Position: sticky detection and polyfill
-const stickyPolyfill: void = (function() {
-    const el: HTMLElement = document.createElement('a');
-    el.style.cssText =
-        'position:sticky; position:-webkit-sticky; position:-ms-sticky;';
-    const isSupported: boolean = el.style.position.indexOf('sticky') !== -1;
-    if (!isSupported) {
-        import('stickyfilljs')
-            .then(Stickyfill => {
-                const stickyElements = Array.prototype.slice.call(
-                    document.querySelectorAll('.cs-sticky-block--no-mobile')
-                );
-
-                if (stickyElements.length > 0) {
-                    if (window.innerWidth > breakpoint.tablet) {
-                        Stickyfill.add(stickyElements);
-                    }
-
-                    // Handling resolution change
-                    let isMobile: boolean;
-                    let isSticky: boolean;
-                    window.onresize = () => {
-                        isMobile = !!(window.innerWidth <= breakpoint.tablet);
-                        isSticky = !!(Stickyfill.stickies.length > 0);
-                        if (!isMobile && !isSticky) {
-                            Stickyfill.add(stickyElements);
-                        } else if (isMobile && isSticky) {
-                            Stickyfill.remove(stickyElements);
-                        }
-                    };
-                }
-            })
-            .catch(err => {
-                // tslint:disable-next-line: no-console
-                console.error(
-                    "Error: couldn't load 'position: sticky' polyfill."
-                );
-            });
-    }
-})();
+// console.log(Sticky);
