c354243485e9255430feaa29a049e5aec231abe6
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2020-02-17 11:44:21 +0100

    Add performance improvements

--- src/utils/idle-deffered.ts
+++ src/utils/idle-deffered.ts
@@ -6,33 +6,36 @@ export interface IdleDeferred extends JQuery.Deferred<void> {
 
 /**
  * Returns a jQuery Deferred object which resolves when browser enters idle state.
- * If the browser doesn't support requestIdleCallback, deferred is resolved immediately.
+ * If the browser doesn't support requestIdleCallback, deferred is resolved on window load.
  * It is possible to force resolving using .force() method.
  *
  * @param timeout Optional timeout for waiting for idle state in milliseconds.
  */
 export default (timeout?: number): IdleDeferred => {
+    const $window = $(window);
     const deferred = $.Deferred() as IdleDeferred;
-    const idleSetter = window.requestIdleCallback;
-    const idleCanceller = window.cancelIdleCallback;
 
-    deferred.force = () => {
-        if (idleCanceller) {
-            idleCanceller(idleCallback);
-        }
+    const callback = () => {
         deferred.resolve();
-
-        return deferred;
     };
 
-    if (!idleSetter) {
-        deferred.resolve();
-        return deferred;
-    }
+    let idleCallback = null;
 
-    const idleCallback = idleSetter(() => {
-        deferred.resolve();
-    }, timeout);
+    deferred.force = () => {
+        if ('cancelIdleCallback' in window) {
+            cancelIdleCallback(idleCallback);
+        } else {
+            $window.off('load', callback);
+        }
+
+        return deferred.resolve();
+    };
+
+    if ('requestIdleCallback' in window) {
+        idleCallback = requestIdleCallback(callback, timeout);
+    } else {
+        $window.on('load', callback);
+    }
 
     return deferred;
 };
