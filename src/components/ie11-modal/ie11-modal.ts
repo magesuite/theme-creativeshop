d42892e4487734e6b9267b335145c9796fc9baf2
Mateusz Kusibab <mateusz.kusibab@creativestyle.pl>
2020-07-24 07:59:17 +0000

    [P2G-1284] fix translations and add enable/disable option in view.xml for IE 11 modal

--- src/components/ie11-modal/ie11-modal.ts
+++ src/components/ie11-modal/ie11-modal.ts
@@ -1,4 +1,6 @@
 import * as $ from 'jquery';
+import viewXml from 'etc/view';
+import deepGet from 'utils/deep-get/deep-get';
 import requireAsync from 'utils/require-async';
 import 'mage/translate';
 
@@ -22,19 +24,26 @@ export interface ie11ModalOptions {
     texts?: object;
 }
 
+/**
+ * ie11Modal component options interface.
+ */
+interface XmlSettings {
+    enabled: boolean;
+}
+
 export default class ie11Modal {
+    protected _isEnabled: XmlSettings;
     protected _$element?: JQuery;
     protected $modalWrapper: JQuery;
     protected _options?: ie11ModalOptions = {
         modalClassname: 'cs-ie11-modal',
         texts: {
-            headline: 'This internet browser is outdated',
-            main: [
-                'This store benefits from security and efficiency (or performance) of modern browsers.',
-                'You are currently using Internet Explorer that is 7 years old and is outdated.',
-                'Please consider installing and using newer browser to have bettter experience and access to all functions of this store.',
-            ],
-            recommendation: 'These are the browsers we recommend:',
+            headline: $.mage.__('This internet browser is outdated'),
+            main: $.mage.__(
+                'This store benefits from security and efficiency (or performance) of modern browsers. You are currently using Internet Explorer that is 7 years old and is outdated. Please consider installing and using newer browser to have better experience and access to all functions of this store.'
+            ),
+            recommendation: $.mage.__('These are the browsers we recommend:'),
+            download: $.mage.__('Download'),
         },
         modalTemplate: '',
         modalOptions: {
@@ -42,6 +51,7 @@ export default class ie11Modal {
             responsive: true,
             clickableOverlay: false,
             modalClass: 'cs-ie11-modal',
+            autoOpen: true,
             buttons: [
                 {
                     text: $.mage.__('I understand'),
@@ -58,18 +68,18 @@ export default class ie11Modal {
         $element?: JQuery<HTMLElement>,
         options?: ie11ModalOptions
     ) {
+        this._isEnabled = deepGet(
+            viewXml,
+            'vars.Magento_Theme.ie11_modal.enabled'
+        );
+
+        if (!this._isEnabled || !this._isIe11()) {
+            return;
+        }
+
         this._$element = $element || $(`.${this._options.modalClassname}`);
         this._options = $.extend({}, this._options, options);
         this.$modalWrapper;
-
-        const ua = navigator.userAgent;
-        if (ua.indexOf('MSIE ') === -1 && ua.indexOf('Trident/') === -1) {
-            return;
-        } else {
-            if (sessionStorage.getItem('magesuite-ie11-modal')) {
-                return;
-            }
-        }
 
         this._createModal();
     }
@@ -79,82 +89,39 @@ export default class ie11Modal {
             return this._options.modalTemplate;
         }
 
-        let content = '';
-        this._options.texts.main.forEach(el => {
-            content += $.mage.__(el) + ' ';
-        });
-
         return `
             <div class="${this._options.modalClassname}__inner">
                 <div class="${this._options.modalClassname}__header">
-                    <img class="${
-                        this._options.modalClassname
-                    }__header-icon" data-bind="attr: {src: require.toUrl('./images/browser/ie-grayscale.svg')}">
+                    <img class="${this._options.modalClassname}__header-icon" data-bind="attr: {src: require.toUrl('./images/browser/ie-grayscale.svg')}">
                     <span class="${this._options.modalClassname}__header-title">
-                        ${$.mage.__(this._options.texts.headline)}
+                        ${this._options.texts.headline}
                     </span>
                 </div>
                 <div class="${this._options.modalClassname}__body">
-                    <p class="${
-                        this._options.modalClassname
-                    }__paragraph">${content}</p>
-                    <p class="${this._options.modalClassname}__paragraph ${
-            this._options.modalClassname
-        }__paragraph--title">
-                        ${$.mage.__(this._options.texts.recommendation)}
+                    <p class="${this._options.modalClassname}__paragraph">${this._options.texts.main}</p>
+                    <p class="${this._options.modalClassname}__paragraph ${this._options.modalClassname}__paragraph--title">
+                        ${this._options.texts.recommendation}
                     </p>
                     <ul class="${this._options.modalClassname}__list">
-                        <li class="${this._options.modalClassname}__list-item ${
-            this._options.modalClassname
-        }__list-item--chrome">
-                            <img class="${
-                                this._options.modalClassname
-                            }__item-logo" data-bind="attr: {src: require.toUrl('./images/browser/chrome.svg')}">
-                            <span class="${
-                                this._options.modalClassname
-                            }__item-name">Google Chrome</span>
-                            <a href="https://www.google.com/chrome/" target="_blank" class="${
-                                this._options.modalClassname
-                            }__item-link">${$.mage.__('Download')}</a>
+                        <li class="${this._options.modalClassname}__list-item ${this._options.modalClassname}__list-item--chrome">
+                            <img class="${this._options.modalClassname}__item-logo" data-bind="attr: {src: require.toUrl('./images/browser/chrome.svg')}">
+                            <span class="${this._options.modalClassname}__item-name">Google Chrome</span>
+                            <a href="https://www.google.com/chrome/" target="_blank" class="${this._options.modalClassname}__item-link">${this._options.texts.download}</a>
                         </li>
-                        <li class="${this._options.modalClassname}__list-item ${
-            this._options.modalClassname
-        }__list-item--firefox">
-                            <img class="${
-                                this._options.modalClassname
-                            }__item-logo" data-bind="attr: {src: require.toUrl('./images/browser/firefox.svg')}">
-                            <span class="${
-                                this._options.modalClassname
-                            }__item-name">Firefox</span>
-                            <a href="https://www.firefox.com/" target="_blank" class="${
-                                this._options.modalClassname
-                            }__item-link">${$.mage.__('Download')}</a>
+                        <li class="${this._options.modalClassname}__list-item ${this._options.modalClassname}__list-item--firefox">
+                            <img class="${this._options.modalClassname}__item-logo" data-bind="attr: {src: require.toUrl('./images/browser/firefox.svg')}">
+                            <span class="${this._options.modalClassname}__item-name">Firefox</span>
+                            <a href="https://www.firefox.com/" target="_blank" class="${this._options.modalClassname}__item-link">${this._options.texts.download}</a>
                         </li>
-                        <li class="${this._options.modalClassname}__list-item ${
-            this._options.modalClassname
-        }__list-item--opera">
-                            <img class="${
-                                this._options.modalClassname
-                            }__item-logo" data-bind="attr: {src: require.toUrl('./images/browser/opera.svg')}">
-                            <span class="${
-                                this._options.modalClassname
-                            }__item-name">Opera</span>
-                            <a href="https://www.opera.com/" target="_blank" class="${
-                                this._options.modalClassname
-                            }__item-link">${$.mage.__('Download')}</a>
+                        <li class="${this._options.modalClassname}__list-item ${this._options.modalClassname}__list-item--opera">
+                            <img class="${this._options.modalClassname}__item-logo" data-bind="attr: {src: require.toUrl('./images/browser/opera.svg')}">
+                            <span class="${this._options.modalClassname}__item-name">Opera</span>
+                            <a href="https://www.opera.com/" target="_blank" class="${this._options.modalClassname}__item-link">${this._options.texts.download}</a>
                         </li>
-                        <li class="${this._options.modalClassname}__list-item ${
-            this._options.modalClassname
-        }__list-item--edge">
-                            <img class="${
-                                this._options.modalClassname
-                            }__item-logo" data-bind="attr: {src: require.toUrl('./images/browser/edge.svg')}">
-                            <span class="${
-                                this._options.modalClassname
-                            }__item-name">Edge</span>
-                            <a href="https://www.microsoft.com/en-us/windows/microsoft-edge/" target="_blank" class="${
-                                this._options.modalClassname
-                            }__item-link">${$.mage.__('Download')}</a>
+                        <li class="${this._options.modalClassname}__list-item ${this._options.modalClassname}__list-item--edge">
+                            <img class="${this._options.modalClassname}__item-logo" data-bind="attr: {src: require.toUrl('./images/browser/edge.svg')}">
+                            <span class="${this._options.modalClassname}__item-name">Edge</span>
+                            <a href="https://www.microsoft.com/en-us/windows/microsoft-edge/" target="_blank" class="${this._options.modalClassname}__item-link">${this._options.texts.download}</a>
                         </li>
                     </ul>
                 </div>
@@ -180,12 +147,25 @@ export default class ie11Modal {
                     this.$modalWrapper
                 );
 
-                this.$modalWrapper.modal('openModal');
                 this.$modalWrapper.on('modalclosed', () => {
                     this._setStorageFlag();
                 });
             }
         );
+    }
+
+    protected _isIe11() {
+        const ua = navigator.userAgent;
+
+        if (ua.indexOf('MSIE ') === -1 && ua.indexOf('Trident/') === -1) {
+            return false;
+        } else {
+            if (sessionStorage.getItem('magesuite-ie11-modal')) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     protected _setStorageFlag() {
