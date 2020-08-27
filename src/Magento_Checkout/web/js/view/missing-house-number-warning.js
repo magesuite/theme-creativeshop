84adb8b1056aae85148369463c204f9069c58d4c
Marta Siewierska-Stańczak <marta.siewierska-stanczak@creativestyle.pl>
2020-08-27 07:16:36 +0000

    Check also second and third street input in checkout when checking for street number

--- src/Magento_Checkout/web/js/view/missing-house-number-warning.js
+++ src/Magento_Checkout/web/js/view/missing-house-number-warning.js
@@ -6,9 +6,15 @@ define(['uiComponent', 'uiRegistry', 'mage/translate'], function(
     'use strict';
     return Component.extend({
         defaults: {
-            streetElement: null,
+            streetElement1: null,
+            streetElement2: null,
+            streetElement3: null,
             listens: {
                 '${ $.provider }:shippingAddress.street.0':
+                    'streetValueChanged',
+                '${ $.provider }:shippingAddress.street.1':
+                    'streetValueChanged',
+                '${ $.provider }:shippingAddress.street.2':
                     'streetValueChanged',
             },
         },
@@ -19,17 +25,47 @@ define(['uiComponent', 'uiRegistry', 'mage/translate'], function(
             registry.async(
                 'checkout.steps.shipping-step.shippingAddress.shipping-address-fieldset.street.0'
             )(function(element) {
-                self.streetElement = element;
+                self.streetElement1 = element || null;
+            });
+
+            registry.async(
+                'checkout.steps.shipping-step.shippingAddress.shipping-address-fieldset.street.1'
+            )(function(element) {
+                self.streetElement2 = element || null;
+            });
+
+            registry.async(
+                'checkout.steps.shipping-step.shippingAddress.shipping-address-fieldset.street.2'
+            )(function(element) {
+                self.streetElement3 = element;
             });
         },
         streetValueChanged: function(value) {
-            if (this.streetElement) {
-                if (!/\d/.test(value)) {
-                    this.streetElement.warn(
+            var lastStreetElement;
+            var combinedText = '';
+
+            if (this.streetElement1) {
+                combinedText = this.streetElement1.value();
+                lastStreetElement = this.streetElement1;
+            }
+
+            if (this.streetElement2) {
+                combinedText = combinedText + this.streetElement2.value();
+                lastStreetElement = this.streetElement2;
+            }
+
+            if (this.streetElement3) {
+                combinedText = combinedText + this.streetElement3.value();
+                lastStreetElement = this.streetElement3;
+            }
+
+            if (lastStreetElement) {
+                if (!/\d/.test(combinedText)) {
+                    lastStreetElement.warn(
                         $t('Do not forget about street number')
                     );
                 } else {
-                    this.streetElement.warn(null);
+                    lastStreetElement.warn(null);
                 }
             }
         },
