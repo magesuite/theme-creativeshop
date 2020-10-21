cd7e28d247e8a103e1ea951c3fdfb92c11d5f051
Tomasz Drabik <tomasz.drabik@creativetyle.pl>
2020-10-21 08:53:02 +0200

    Add products list import to page entries

--- src/entries/checkout.ts
+++ src/entries/checkout.ts
@@ -46,6 +46,7 @@ import 'components/page-title';
 import 'components/page-bottom';
 import 'components/price-box';
 import 'components/product-tile';
+import 'components/products-list';
 import 'components/pwa-notification-panel';
 import 'components/slider';
 import 'components/social-media-list';
--- src/entries/products-grid.ts
+++ src/entries/products-grid.ts
@@ -1,5 +1,4 @@
 import 'components/products-grid';
-import 'components/products-list';
 
 // tslint:disable-next-line:no-empty
 export function ccProductsGrid() {}
