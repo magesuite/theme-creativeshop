af24bcd6f9e74adb6c77da8c1d534a8769409a40
Maciej Borkowski <maciej.borkowski@creativestyle.pl>
2021-03-16 15:50:34 +0100

    hotfix: adds missing 'isMobile' import into deivce-detection component

--- src/components/device-detection/mobile.ts
+++ src/components/device-detection/mobile.ts
@@ -1,3 +1,5 @@
+import * as isMobile from 'isMobile';
+
 // Sometimes there is a need to apply different styling for mobile/tablet devices and body class is necessary
 if (isMobile.any) {
     document.body.classList.add('is-mobile');
