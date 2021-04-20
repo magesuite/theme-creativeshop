37acfb9cbda2da772ebd463207c79432f948ba2b
Marta Siewierska-Stańczak <marta.siewierska-stanczak@creativestyle.pl>
2021-04-20 16:01:52 +0000

    Revert "feat: [KRG-1676] Scroll to payment method error after is is shown"
    This reverts commit 3b0416941388e9bad605a968061a01e96b3cee4b

--- src/Magento_Ui/web/js/view/messages-ext.js
+++ src/Magento_Ui/web/js/view/messages-ext.js
@@ -5,31 +5,6 @@ define(['ko', 'jquery'], function(ko, $) {
     'use strict';
     return function(Messages) {
         return Messages.extend({
-            /** @inheritdoc */
-            initialize: function() {
-                this._super();
-
-                if (
-                    this.index.includes(
-                        'checkout.steps.billing-step.payment.payments-list'
-                    )
-                ) {
-                    this.messageContainer.errorMessages.subscribe(function() {
-                        if ($('.payment-method._active').length) {
-                            $('html, body').animate(
-                                {
-                                    scrollTop: $(
-                                        '.payment-method._active'
-                                    ).offset().top,
-                                },
-                                800
-                            );
-                        }
-                    });
-                }
-
-                return this;
-            },
             /**
              * @param {Boolean} isHidden
              */
