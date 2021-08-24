0040fb8e52d704db5bf3bb9a478a7cedf44ec30f
Krzysztof Kula <krzysztof.kula@creativestyle.pl>
2021-08-24 08:26:19 +0000

    refactor(page-scroll)!: P2G-1610 To the top jumpmark missing

--- src/components/page-scroll/index.ts
+++ src/components/page-scroll/index.ts
@@ -1,15 +1,7 @@
-import * as $ from 'jquery';
 import PageScroll from 'components/page-scroll/page-scroll';
 import 'components/page-scroll/page-scroll.scss';
 
 /**
  * PageScroll component initialization
  */
-if ($('.brands-index-all').length > 0) {
-    new PageScroll({
-        targetElementSelector: '.cs-brands-index__icons',
-        hideAfterSelector: '.cs-brands-index__list',
-    });
-} else {
-    new PageScroll();
-}
+new PageScroll();
--- src/components/page-scroll/page-scroll.ts
+++ src/components/page-scroll/page-scroll.ts
@@ -2,56 +2,33 @@ import * as $ from 'jquery';
 
 interface IPageScrollSettings {
     /**
-     * Defines class of component
-     * @default {cs-page-scroll}
+     * Defines selector of component
+     * @default {'.cs-page-scroll'}
      * @type {String}
      */
-    componentClass?: string;
+    componentSelector?: string;
 
     /**
-     * Defines class for component by which
-     * component is initially hidden
-     * Class itself is set in XML config.
-     * @default {cs-visually-hidden}
+     * Defines selector of component's button
+     * @default {'.cs-page-scroll__button'}
      * @type {String}
      */
-    componentHiddenClass?: string;
+    buttonSelector?: string;
 
     /**
-     * Defines class of component's button
-     * @default {cs-page-scroll__button}
+     * Defines selector of element which observe. It's trigger show / hide function
+     * @default {'body'}
      * @type {String}
      */
-    componentButtonClass?: string;
+    observeElementSelector?: string;
 
     /**
      * Defines event type for scroll action to begin
      * Supported options: click
-     * @default {click}
+     * @default {'click'}
      * @type {String}
      */
     actionEvent?: string;
-
-    /**
-     * Defines the target element to observe to display / hide scroll button
-     * @default {'.cs-container--top-bar'}
-     * @type {String}
-     */
-    targetElementSelector?: string;
-
-    /**
-     * Defines offset on target element on mobiles / tablet devices
-     * @default {0}
-     * @type {number}
-     */
-    targetElementOffsetMobile?: any;
-
-    /**
-     * Defines offset on target element on desktop devices
-     * @default {0}
-     * @type {number}
-     */
-    targetElementOffsetDesktop?: any;
 
     /**
      * Defines `top` value for scrollTo method
@@ -62,17 +39,15 @@ interface IPageScrollSettings {
 
     /**
      * Defines `behavior` value for scrollTo method
-     * @default {smooth}
+     * @default {'smooth'}
      * @type {String}
      */
     scrollTopBehavior?: string;
 
     /**
-     * Defines breakpoint when change offset value between mobile / desktop
-     * @default {laptop}
-     * @type {String}
+     * Defines options for IntersectionObserver
      */
-    breakpoint?: number;
+    observerOptions?: object;
 }
 
 /**
@@ -84,7 +59,6 @@ interface IPageScrollSettings {
 export default class PageScroll {
     private _$component: JQuery<HTMLElement>;
     private _$button: JQuery<HTMLElement>;
-    private _offset: string;
     private _options?: IPageScrollSettings;
     private _observer?: IntersectionObserver;
 
@@ -97,16 +71,17 @@ export default class PageScroll {
             true,
             {},
             {
-                componentClass: 'cs-page-scroll',
-                componentHiddenClass: 'cs-visually-hidden',
-                componentButtonClass: 'cs-page-scroll__button',
+                componentSelector: '.cs-page-scroll',
+                buttonSelector: '.cs-page-scroll__button',
+                observeElementSelector: 'body',
                 actionEvent: 'click',
-                targetElementSelector: '.cs-container--top-bar',
-                targetElementOffsetMobile: '0px',
-                targetElementOffsetDesktop: '0px',
                 scrollTopPos: 0,
                 scrollTopBehavior: 'smooth',
-                breakpoint: 'laptop',
+                observerOptions: {
+                    root: null,
+                    rootMargin: '0px',
+                    threshold: 0.5,
+                },
             },
             options
         );
@@ -118,85 +93,29 @@ export default class PageScroll {
      * Initiates component.
      */
     protected _init(): void {
-        if (
-            !('IntersectionObserver' in window) ||
-            $(`.${this._options.componentClass}`).length <= 0
-        ) {
+        this._$component = $(this._options.componentSelector);
+        this._$button = $(this._options.buttonSelector);
+
+        if (!('IntersectionObserver' in window) || !this._$component) {
             return;
         }
 
-        this._$component = $(`.${this._options.componentClass}`);
-        this._$button = $(`.${this._options.componentButtonClass}`);
-
-        if (this._$button.length) {
-            this._$component.removeClass(
-                `${this._options.componentHiddenClass}`
-            );
-            this._offset = this._setOffset(window.breakpoint.current);
-            this._setObserver(window.breakpoint.current);
+        if (this._$component && this._$button) {
+            this._setObserver();
             this._setEvent();
         }
-
-        if (
-            this._options.targetElementOffsetMobile !==
-            this._options.targetElementOffsetDesktop
-        ) {
-            this._watch();
-        }
-    }
-
-    /**
-     * Return offset value (targetElementOffsetMobile/targetElementOffsetDesktop) depends on current breakpoint value
-     * @param breakpointValue
-     * @returns {String}
-     */
-    protected _setOffset(breakpointValue): string {
-        return breakpointValue >= window.breakpoint[this._options.breakpoint]
-            ? this._options.targetElementOffsetDesktop
-            : this._options.targetElementOffsetMobile;
     }
 
     /**
      * Set & enable observer
-     * @param breakpointValue {Number} breakpoint value
      */
-    protected _setObserver(breakpointValue): void {
-        const observeOptions = {
-            root: null,
-            rootMargin: this._setOffset(breakpointValue),
-            treshold: 1,
-        };
-
+    protected _setObserver(): void {
         this._observer = new IntersectionObserver(e => {
             !e[0].isIntersecting ? this._showButton() : this._hideButton();
-        }, observeOptions);
+        }, this._options.observerOptions);
 
-        this._observer.observe($(this._options.targetElementSelector)[0]);
-    }
-
-    /**
-     * Remove observer
-     */
-    protected _unsetObserver(): void {
-        this._observer.unobserve($(this._options.targetElementSelector)[0]);
-    }
-
-    /**
-     * Reinitialize observer on breakpoint change if targetElementOffsetMobile and targetElementOffsetDesktop options has different value
-     */
-    protected _watch(): void {
-        $(window).on(
-            'breakpointChange',
-            (e: JQuery.Event, newBreakpoint: number): void => {
-                if (
-                    this._offset.toLowerCase() !==
-                    this._setOffset(newBreakpoint).toLowerCase()
-                ) {
-                    this._unsetObserver();
-                    this._setObserver(newBreakpoint);
-                    this._offset = this._setOffset(newBreakpoint);
-                }
-            }
+        this._observer.observe(
+            document.querySelector(this._options.observeElementSelector)
         );
     }
 
@@ -205,9 +124,7 @@ export default class PageScroll {
      * Shows button by adding component button class with `--visible` modifier
      */
     protected _showButton(): void {
-        this._$button.addClass(
-            `${this._options.componentButtonClass}--visible`
-        );
+        this._$button.addClass('visible');
     }
 
     /**
@@ -215,9 +132,7 @@ export default class PageScroll {
      * Hides button by removing component button class with `--visible` modifier
      */
     protected _hideButton(): void {
-        this._$button.removeClass(
-            `${this._options.componentButtonClass}--visible`
-        );
+        this._$button.removeClass('visible');
     }
 
     /**
@@ -236,7 +151,7 @@ export default class PageScroll {
     }
 
     /**
-     * Monitors `click` event on button.
+     * Monitors `this._options.actionEvent` event on button and invoke scrollToTop
      */
     protected _setEvent(): void {
         this._$button.on(this._options.actionEvent, (e: JQuery.Event): void =>
