80d0d7beab926033bd64e5ca3725f43e07f018ec
Marta Siewierska-Stańczak <marta.siewierska-stanczak@creativestyle.pl>
2022-03-04 12:28:16 +0100

    fix: [mgs-4284] Prevent hiding of autocomplete when native keyboard is closed

--- src/Smile_ElasticsuiteCore/web/js/form-mini-ext.js
+++ src/Smile_ElasticsuiteCore/web/js/form-mini-ext.js
@@ -10,12 +10,26 @@ define(['jquery'], function($) {
             _create: function() {
                 var self = this;
                 this._super();
+
+                // Blur handle is added twice, first in original Magento widget,
+                // then in smile mixin
+                // We want to be sure that we add our custom blur handle only once and remove other handles
+                this.element.off('blur'),
+                this._blur();
+
                 $('body').on('click', function(event) {
                     if (!$(event.target).closest(this.autoComplete).length) {
                         self._resetResponseList(true);
                         self.autoComplete.hide();
                     }
                 });
+
+                this.isTouchDevice = this._isTouchDevice();
+            },
+            _isTouchDevice() {
+                return (('ontouchstart' in window) ||
+                   (navigator.maxTouchPoints > 0) ||
+                   (navigator.msMaxTouchPoints > 0));
             },
             _getSectionHeader: function(type, data) {
                 var header = this._super(type, data);
@@ -58,6 +72,36 @@ define(['jquery'], function($) {
                     Boolean(!all && this.responseList.indexList.length)
                 );
             },
+            /**
+             * Override blur event handle in order to not close autocomplete on touch devices
+             * When native keyboard is closed input can be focused out 
+             * which make impossible to choose item from autocomplete list.
+             * To close autocomplete on touch device click outside autocomplete can be used 
+             * as well as click on cs-header-search__close which hides whole search
+             */
+            _blur: function() {
+                this.element.on('blur', $.proxy(function (e) {
+                    if (!this.searchLabel.hasClass('active')) {
+                        return;
+                    }
+                    setTimeout($.proxy(function () {
+                        if (this.isTouchDevice) {
+                            this.element.trigger('focus');
+                            this.setActiveState(true);
+                            return;
+                        };
+
+                        if (this.autoComplete.is(':hidden')) {
+                            this.setActiveState(false);
+                        } else {
+                            this.element.trigger('focus');
+                        }
+                        this.autoComplete.hide();
+                        $('#search').blur();
+                        this._updateAriaHasPopup(false);
+                    }, this),250);
+                }, this));
+            }
         });
 
         return $.smileEs.quickSearch;
