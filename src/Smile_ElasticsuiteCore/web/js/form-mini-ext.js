31e3b7f8e2efec983bb982dc1389aae3ca0be485
Marta Siewierska-Stańczak <marta.siewierska-stanczak@creativestyle.pl>
2022-03-18 12:16:09 +0100

    fix: [MGS-4326] Do not scroll page under autocomplete dropdown on mobile

--- src/Smile_ElasticsuiteCore/web/js/form-mini-ext.js
+++ src/Smile_ElasticsuiteCore/web/js/form-mini-ext.js
@@ -73,6 +73,12 @@ define(['jquery'], function($) {
                     'active-popup',
                     Boolean(!all && this.responseList.indexList.length)
                 );
+
+                console.log(Boolean(!all && this.responseList.indexList.length));
+                $('html').toggleClass(
+                    'autocomplete-dropdown-visible',
+                    Boolean(!all && this.responseList.indexList.length)
+                );
             },
             /**
              * Override blur event handle in order to not close autocomplete on touch devices
