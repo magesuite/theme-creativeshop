49634cec6055fb4c4371d6a75df616be3301c412
Katarzyna Berbeka <katarzyna.berbeka@creativestyle.pl>
2023-03-28 08:30:27 +0000

    feat: [TOYS-674] Initialize dailydeal on tiles in carousels rendered dynamically

--- src/components/dailydeal/index.ts
+++ src/components/dailydeal/index.ts
@@ -27,21 +27,37 @@ $(`.${ns}dailydeal--tile`).each(function (): void {
     });
 });
 
-const $minicartCarousel = $('.cs-minicart__carousel');
+// Multiple carousels are rendered dinamically.
+// In order to initialise daily deal feature properly, the initialization must take place after contentUpdated event
 
-// Minicart carousel is initialised after ajax call.
-// In order to initialize daily deal feature properly, the initialization must take place after contentUpdated event
-if ($minicartCarousel.length) {
-    $minicartCarousel.on('contentUpdated', function () {
-        $minicartCarousel.find(`.${ns}dailydeal--tile`).each(function (): void {
-            setTimeout(() => {
-                new Dailydeal($(this), {
-                    namespace: ns,
-                    updateLabels: true,
+const minicartCarousel = '.cs-minicart__carousel'; // mgs carousel
+const visitorRecommender = '[id^="elasticsuite-recommender-container"]'; // elastic widget
+const comparedProductsarousel = '.widget.block-compared-products-grid'; // elastic widget
+const viewedProductsarousel = '.admin__data-grid-outer-wrap'; // magento widget
+const carouselsRenderedDynamically: any = [];
+
+carouselsRenderedDynamically.push(
+    minicartCarousel,
+    visitorRecommender,
+    comparedProductsarousel,
+    viewedProductsarousel
+);
+
+$(carouselsRenderedDynamically).each(function (i, element) {
+    if ($(element).length) {
+        $(element).on('contentUpdated', function () {
+            $(element)
+                .find(`.${ns}dailydeal--tile`)
+                .each(function (): void {
+                    setTimeout(() => {
+                        new Dailydeal($(this), {
+                            namespace: ns,
+                            updateLabels: true,
+                        });
+                    });
                 });
-            });
         });
-    });
-}
+    }
+});
 
 export { Dailydeal };
