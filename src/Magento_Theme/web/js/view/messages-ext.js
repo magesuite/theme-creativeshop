0e086881a72fd40aec4bdd1a4dce747c6ac1810b
Rafał Jękosz <rafaljekosz@local.local>
2024-11-05 11:25:38 +0100

    fix(messages): [FZ-399] Fix message behaviour. Add setting for message visibility duration

---
+++ src/Magento_Theme/web/js/view/messages-ext.js
@@ -0,0 +1,28 @@
+/**
+ * Mixin added in order to prevent message being displayed on other URL after load, when user hits "back" button before message was removed
+ *
+ * Messages observable is now cleaned on beforeunload event
+ *
+ * Aligned with Magento 2.4.7 on 05/11/2024
+ */
+define(['jquery'], function ($, wrapper) {
+    'use strict';
+
+    var mixin = {
+        initialize: function () {
+            this._super();
+
+            this._attachUnloadEvent();
+        },
+
+        _attachUnloadEvent: function () {
+            $(window).on('beforeunload', () => {
+                this.purgeMessages();
+            });
+        },
+    };
+
+    return function (target) {
+        return target.extend(mixin);
+    };
+});
