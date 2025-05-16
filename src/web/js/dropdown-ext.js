c101932fe82f3beea979829b0edd1e349e75922b
Bartłomiej Podgórski <bartlomiej.podgorski@creativestyle.pl>
2025-05-16 10:36:44 +0000

    fix: [NKD-4676] A11y, Minicart - clear ARC errors regarding 1.3.2 criterion

--- src/web/js/dropdown-ext.js
+++ src/web/js/dropdown-ext.js
@@ -2,14 +2,16 @@
  * Origin: Native M2 Collapsible Widget
  * Modification type: extend
  * Reasons:
- * - close dropdown when navigation flyout is opened [1]
- * - provide proper aria-label for dropdown dialog due to accessibility reasons: [2]
+ * - [1] close dropdown when navigation flyout is opened
+ * - [2] provide proper aria-label for dropdown dialog due to accessibility reasons:
  *   * dropdown dialog content should have element with "data-labelledby='true'" attribute and "id",
  *     then "id" value will be used as "aria-labelledby" attribute for dialog element
  *   * if there is no element with "data-labelledby" attribute, then default "aria-label" attribute will be used
  *   * if dialogTitle is specified, it's value will be used as "aria-label" attribute
- * - remove unwanted "tabindex = -1" attribute from dialog element when it is being added to DOM [3]
- * - generate unique id for dropdown dialog element if it does not have one [4]
+ * - [3] remove unwanted "tabindex = -1" attribute from dialog element when it is being added to DOM
+ * - [4] generate unique id for dropdown dialog element if it does not have one
+ * - [5] introduced option "removeAccessibilityAttributes" to remove all accessibility attributes from dialog element
+ *       as we are using custom dialog (offcanvas) implementation
  *
  * Aligned with Magento 2.4.7 on 02/2025
  */
@@ -18,6 +20,11 @@ define(['jquery', 'mage/translate'], function ($, $t) {
 
     return function (mageDropdownDialog) {
         $.widget('mage.dropdownDialog', mageDropdownDialog, {
+            options: {
+                /** edit [5] start */
+                removeAccessibilityAttributes: false,
+                /** edit [5] end */
+            },
             _create: function () {
                 /** edit [4] start */
                 const uniqueId =
@@ -58,6 +65,15 @@ define(['jquery', 'mage/translate'], function ($, $t) {
 
                 this.uiDialog.attr(ariaAttributes);
                 /** edit [2] end */
+
+                /** edit [5] start */
+                if (this.options.removeAccessibilityAttributes) {
+                    this.uiDialog
+                        .removeAttr('aria-labelledby')
+                        .removeAttr('aria-describedby')
+                        .removeAttr('role');
+                }
+                /** edit [5] end */
             },
         });
 
