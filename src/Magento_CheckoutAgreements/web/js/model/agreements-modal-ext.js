b5e9e96623121b1fea42254e08cb22db67601fba
patrycja niestroj <patrycja.niestroj@gmail.com>
2025-06-05 20:00:15 +0200

    feat: [RK-1876] A11Y - add title to agreements modal

---
+++ src/Magento_CheckoutAgreements/web/js/model/agreements-modal-ext.js
@@ -0,0 +1,42 @@
+/**
+ * Changes in comparison to source file:
+ * - modified: createModal to add title (l:24),
+ * Aligned with Magento 2.4.7 in 06/2025
+ */
+define(['jquery', 'Magento_Ui/js/modal/modal', 'mage/translate'], function (
+    $,
+    modal,
+    $t
+) {
+    'use strict';
+
+    return function (agreementsModal) {
+        agreementsModal.createModal = function (element) {
+            var options;
+
+            this.modalWindow = element;
+            options = {
+                type: 'popup',
+                modalClass: 'agreements-modal',
+                responsive: true,
+                innerScroll: true,
+                trigger: '.show-modal',
+                title: $t('Terms and Conditions'),
+                buttons: [
+                    {
+                        text: $t('Close'),
+                        class: 'action secondary action-hide-popup',
+
+                        /** @inheritdoc */
+                        click: function () {
+                            this.closeModal();
+                        },
+                    },
+                ],
+            };
+            modal(options, $(this.modalWindow));
+        };
+
+        return agreementsModal;
+    };
+});
