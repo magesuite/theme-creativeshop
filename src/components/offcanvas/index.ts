272cb2bc08652f9373bf6213f7a835eeb6de58c2
Bartłomiej Podgórski <bartlomiej.podgorski@creativestyle.pl>
2025-06-11 05:26:13 +0000

    fix: [NKD-4763] A11y, Offcanvas nav - focus on list's first child

--- src/components/offcanvas/index.ts
+++ src/components/offcanvas/index.ts
@@ -5,4 +5,6 @@ export default new offcanvas(null, {
     className: 'cs-offcanvas--navigation',
     triggerClassName: 'cs-offcanvas-toggle',
     bodyOpenClass: 'navigation-offcanvas-open',
+    initiallyFocusableElement:
+        '.cs-offcanvas-navigation__list--current .cs-offcanvas-navigation__link',
 });
