28392d2fb8631460d547b5c16e06c3f734bc00de
patrycja niestroj <patrycja.niestroj@gmail.com>
2025-08-13 14:36:41 +0200

    fix: [KRA-2234] revert of map controls change

--- src/MageSuite_StoreLocator/web/js/store-locator/store-locator.ts
+++ src/MageSuite_StoreLocator/web/js/store-locator/store-locator.ts
@@ -83,10 +83,6 @@ export default class StoreLocator {
             mapTypeControl: false, // see official google documentation
             streetViewControl: false, // see official google documentation
             fullscreenControl: false, // see official google documentation,
-            cameraControl: true,
-            cameraControlOptions: {
-                position: google.maps.ControlPosition.RIGHT_TOP,
-            },
         },
         basicZoom: 13, // basic zoom (also for small desktop/tablet and mobile - see 2 options below) is set when the map center on specific location (for example when the user is geolocalized or when we pan to a specific store or a location)
         basicZoomSmallDesktop: 12,
