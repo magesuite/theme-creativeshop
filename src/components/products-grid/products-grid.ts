0d5ba9def79cb2467af93556012011d019f62bc9
Dominik Kwaczynski <dominik.kwaczynski@creativestyle.pl>
2025-09-08 12:15:46 +0200

    feat [FR-610] Adjust products grid

---
+++ src/components/products-grid/products-grid.ts
@@ -0,0 +1,68 @@
+export type ProductsGridOptions = {
+    itemSelector: string;
+    minItemHeight: number;
+};
+
+export class ProductsGrid {
+    protected element: HTMLElement;
+    protected options: ProductsGridOptions = {
+        itemSelector: '.cs-products-grid__item',
+        minItemHeight: 200,
+    };
+
+    protected eventListeners = {
+        breakpointChange: this.onBreakpointChange.bind(this),
+    };
+
+    constructor(
+        element: HTMLElement,
+        config: Partial<ProductsGridOptions> = {}
+    ) {
+        this.element = element;
+        if (!this.element) {
+            console.error('ProductsGrid: Element not found');
+            return;
+        }
+
+        this.options = {
+            ...this.options,
+            ...config,
+        };
+
+        this.handleItemsVisibility();
+        this.attachEventListeners();
+    }
+
+    protected attachEventListeners() {
+        document.addEventListener(
+            'breakpointChange',
+            this.eventListeners.breakpointChange
+        );
+    }
+
+    public destroy() {
+        document.removeEventListener(
+            'breakpointChange',
+            this.eventListeners.breakpointChange
+        );
+        this.element = null;
+    }
+
+    protected onBreakpointChange() {
+        this.handleItemsVisibility();
+    }
+
+    protected handleItemsVisibility() {
+        this.element
+            .querySelectorAll<HTMLElement>(this.options.itemSelector)
+            .forEach((item) => {
+                if (item.offsetHeight < this.options.minItemHeight) {
+                    item.setAttribute('aria-hidden', 'true');
+                    item.setAttribute('inert', '');
+                } else {
+                    item.removeAttribute('aria-hidden');
+                    item.removeAttribute('inert');
+                }
+            });
+    }
+}
