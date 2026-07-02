0fc0c9b8257419e2f4f72a00460a957075d44d18
Bartłomiej Podgórski <bartlomiej.podgorski@creativestyle.pl>
2026-07-02 11:57:22 +0000

    feat: [PROS-291] Global messages deletation improved

--- src/Magento_Theme/web/js/view/messages-ext.js
+++ src/Magento_Theme/web/js/view/messages-ext.js
@@ -1,11 +1,12 @@
 /**
- * Mixin added in order to prevent message being displayed on other URL after load, when user hits "back" button before message was removed
- *
- * Messages observable is now cleaned on beforeunload event
+ * Mixin added in order to:
+ * - prevent message being displayed on other URL after load, when user hits "back" button before message was removed
+ *   (messages observable is cleaned on beforeunload event)
+ * - remove a single message from the markup when it is closed (manually or auto-hidden)
  *
  * Aligned with Magento 2.4.9 in 05/2026
  */
-define(['jquery'], function ($, wrapper) {
+define(['jquery', 'Magento_Customer/js/customer-data'], function ($, customerData) {
     'use strict';
 
     var mixin = {
@@ -18,6 +19,18 @@ define(['jquery'], function ($, wrapper) {
         _attachUnloadEvent: function () {
             $(window).on('beforeunload', () => {
                 this.purgeMessages();
+            });
+        },
+
+        removeCookieMessage: function (message) {
+            this.cookieMessagesObservable(
+                this.cookieMessagesObservable().filter((item) => item !== message)
+            );
+        },
+
+        removeMessage: function (message) {
+            customerData.set('messages', {
+                messages: this.messages().messages.filter((item) => item !== message),
             });
         },
     };
